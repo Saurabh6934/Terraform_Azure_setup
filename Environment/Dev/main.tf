a7445b17cb9f85af66266fc6f020e0d497181894
Saurabh Patel <saurabhpatel320@gmail.com>
2025-11-21 10:32:32 +0000

    Updated main.tf

--- Environment/Dev/main.tf
+++ Environment/Dev/main.tf
@@ -4,16 +4,19 @@ module "resource_groups" {
 }
 
 module "virtual_network_subnets" {
+  depends_on = [module.resource_groups]
   source = "../../Modules/Vnet"
   vnets  = var.vnets
 }
 
 module "Key_vault" {
+  depends_on = [module.resource_groups]
   source     = "../../Modules/Key_Vault"
   key_vaults = var.key_vaults
 }
 
 module "public_ip" {
+  depends_on = [module.resource_groups]
   source     = "../../Modules/Public_Ip"
   public_ips = var.public_ips
 }
